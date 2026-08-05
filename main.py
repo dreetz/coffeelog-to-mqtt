b51714d9e54760a2e4478b6da76ad4e1bfd7657a
Dennis Reetz <dennis.reetz@gmail.com>
2026-08-05 08:25:48 +0200

    fix: add error handling for API calls

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import paho.mqtt.publish as publish
 import datetime
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from httpx import ConnectError
 
 
 class Settings(BaseSettings):
@@ -19,97 +20,122 @@ settings = Settings()
 
 def read_total(auth: dict[str, str | None]) -> None:
     """Publish the total amount of coffee."""
-    r = httpx.get(f"{settings.coffeelog_api}/actions/count/total")
-    if r.status_code == 200:
-        if type(r.json()) == int:
-            publish.single(
-                "iot/coffee/count",
-                r.json(),
-                hostname=settings.mqtt_hostname,
-                auth=auth,
-            )
-            publish.single(
-                "iot/coffee/count/total",
-                r.json(),
-                hostname=settings.mqtt_hostname,
-                auth=auth,
-            )
+    try:
+        r = httpx.get(f"{settings.coffeelog_api}/actions/count/total")
+        if r.status_code == 200:
+            if type(r.json()) == int:
+                publish.single(
+                    "iot/coffee/count",
+                    r.json(),
+                    hostname=settings.mqtt_hostname,
+                    auth=auth,
+                )
+                publish.single(
+                    "iot/coffee/count/total",
+                    r.json(),
+                    hostname=settings.mqtt_hostname,
+                    auth=auth,
+                )
+    except ConnectError as exc:
+        print(f"Error while fetching the data from API: {exc}")
+    except Exception as exc:
+        print(f"Error in read_total: {exc}")
 
 
 def read_total_user(auth: dict[str, str | None], user: str) -> None:
     """Publish the total amount of coffee per user."""
-    r = httpx.get(f"{settings.coffeelog_api}/actions/count/total/{user}")
-    if r.status_code == 200:
-        if type(r.json()) == int:
-            publish.single(
-                f"iot/coffee/count/total/{user}",
-                r.json(),
-                hostname=settings.mqtt_hostname,
-                auth=auth,
-            )
+    try:
+        r = httpx.get(f"{settings.coffeelog_api}/actions/count/total/{user}")
+        if r.status_code == 200:
+            if type(r.json()) == int:
+                publish.single(
+                    f"iot/coffee/count/total/{user}",
+                    r.json(),
+                    hostname=settings.mqtt_hostname,
+                    auth=auth,
+                )
+    except ConnectError as exc:
+        print(f"Error while fetching the data from API: {exc}")
+    except Exception as exc:
+        print(f"Error in read_total_user: {exc}")
 
 
 def read_total_today(auth: dict[str, str | None]) -> None:
     """Publish the total amount of coffee of today."""
-    r = httpx.get(f"{settings.coffeelog_api}/actions/count/today")
-    if r.status_code == 200:
-        if type(r.json()) == int:
-            publish.single(
-                f"iot/coffee/count/today",
-                r.json(),
-                hostname=settings.mqtt_hostname,
-                auth=auth,
-            )
+    try:
+        r = httpx.get(f"{settings.coffeelog_api}/actions/count/today")
+        if r.status_code == 200:
+            if type(r.json()) == int:
+                publish.single(
+                    f"iot/coffee/count/today",
+                    r.json(),
+                    hostname=settings.mqtt_hostname,
+                    auth=auth,
+                )
+    except ConnectError as exc:
+        print(f"Error while fetching the data from API: {exc}")
+    except Exception as exc:
+        print(f"Error in read_total_today: {exc}")
 
 
 def read_total_today_user(auth: dict[str, str | None], user: str) -> None:
     """Publish the total amount of coffee of today per given user."""
-    r = httpx.get(f"{settings.coffeelog_api}/actions/count/today/{user}")
-    if r.status_code == 200:
-        if type(r.json()) == int:
-            publish.single(
-                f"iot/coffee/count/today/{user}",
-                r.json(),
-                hostname=settings.mqtt_hostname,
-                auth=auth,
-            )
+    try:
+        r = httpx.get(f"{settings.coffeelog_api}/actions/count/today/{user}")
+        if r.status_code == 200:
+            if type(r.json()) == int:
+                publish.single(
+                    f"iot/coffee/count/today/{user}",
+                    r.json(),
+                    hostname=settings.mqtt_hostname,
+                    auth=auth,
+                )
+    except ConnectError as exc:
+        print(f"Error while fetching the data from API: {exc}")
+    except Exception as exc:
+        print(f"Error in read_total_today_user: {exc}")
 
 
 def read_loaded_coffee(auth: dict[str, str | None]):
     """Publish the information about the coffee set up in the machine."""
-    r = httpx.get(f"{settings.coffeelog_api}/coffee/latest")
-    if r.status_code == 200:
-        data = r.json()
-        publish.single(
-            f"iot/coffee/loaded/coffee",
-            data["coffee_name"],
-            hostname=settings.mqtt_hostname,
-            auth=auth,
-        )
-        publish.single(
-            f"iot/coffee/loaded/roasting_facility",
-            data["roasting_facility"],
-            hostname=settings.mqtt_hostname,
-            auth=auth,
-        )
-        publish.single(
-            f"iot/coffee/loaded/country_of_origin",
-            data["country_of_origin"],
-            hostname=settings.mqtt_hostname,
-            auth=auth,
-        )
-        publish.single(
-            f"iot/coffee/loaded/open_date",
-            data["open_date"],
-            hostname=settings.mqtt_hostname,
-            auth=auth,
-        )
-        publish.single(
-            f"iot/coffee/loaded/roast_date",
-            data["roast_date"],
-            hostname=settings.mqtt_hostname,
-            auth=auth,
-        )
+    try:
+        r = httpx.get(f"{settings.coffeelog_api}/coffee/latest")
+        if r.status_code == 200:
+            data = r.json()
+            publish.single(
+                f"iot/coffee/loaded/coffee",
+                data["coffee_name"],
+                hostname=settings.mqtt_hostname,
+                auth=auth,
+            )
+            publish.single(
+                f"iot/coffee/loaded/roasting_facility",
+                data["roasting_facility"],
+                hostname=settings.mqtt_hostname,
+                auth=auth,
+            )
+            publish.single(
+                f"iot/coffee/loaded/country_of_origin",
+                data["country_of_origin"],
+                hostname=settings.mqtt_hostname,
+                auth=auth,
+            )
+            publish.single(
+                f"iot/coffee/loaded/open_date",
+                data["open_date"],
+                hostname=settings.mqtt_hostname,
+                auth=auth,
+            )
+            publish.single(
+                f"iot/coffee/loaded/roast_date",
+                data["roast_date"],
+                hostname=settings.mqtt_hostname,
+                auth=auth,
+            )
+    except ConnectError as exc:
+        print(f"Error while fetching the data from API: {exc}")
+    except Exception as exc:
+        print(f"Error in read_loaded_coffee: {exc}")
 
 
 if __name__ == "__main__":
